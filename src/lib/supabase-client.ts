af56c01a7ca925401d4f0bfab1bf5500d2aca4a5
iancamps90 <iancamps90@gmail.com>
2025-11-12 21:24:02 +0100

    fix: supabase cookie session sync for production

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -2,11 +2,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://uqigoqmgmptrpxymylvd.supabase.co";
-const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxaWdvcW1nbXB0cnB4eW15bHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NjI1MzYsImV4cCI6MjA3ODUzODUzNn0.J0tbjncdhLJIcSwTELNFRPpt9X02r7tZBgP-dBTY8wc";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabaseClient = createClient(supabaseUrl, supabaseKey, {
   auth: {
